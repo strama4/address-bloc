1540757da4347fd9e954100621f659c7c6b83cc7
Luke Strama <strama@ualberta.ca>
2018-10-19 09:09:33 -0600

    testing-assignment

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -13,4 +13,9 @@ describe("MenuController", () => {
             expect(this.menu.getContactCount()).toBe(1);
         })
     });
+    describe("#remindMe()", () => {
+        it("should return 'Learning is a life-long pursuit'", () => {
+            expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit');
+        })
+    })
 });
